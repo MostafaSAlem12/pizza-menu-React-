f62bc0851f862a85d3542827b0f4c138dee27f44
Mostafa Salem Ali <mostafa.salem.ali@outlook.com>
2024-02-07 09:12:02 +0200

    starter files

--- src/index.js
+++ src/index.js
@@ -5,11 +5,21 @@ function App() {
     return <h1>Hello wORLD</h1>
 }
 
+function Pizza() {
+    return (
+        <>
+        <h2>Pizza</h2>
+        </>
+    )
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     
     <React.StrictMode>
         <App />
+        <Pizza />
+
     </React.StrictMode>
 )
 
