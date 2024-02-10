dae3f58aa9a0e56bffc1937d109d6b9e55dd7bb4
Mostafa Salem Ali <mostafa.salem.ali@outlook.com>
2024-02-10 13:35:11 +0200

    pizzaObj

--- src/index.js
+++ src/index.js
@@ -103,7 +103,7 @@ function Footer() {
 function Pizza(props) {
   return (
     <li className="pizza">
-    <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
+    <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name}/>
       <div>
         <h3>{props.pizzaObj.name}</h3>
         <p>{props.pizzaObj.ingredients}</p>
